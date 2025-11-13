f3d235236df10af7e75d17c5f1abb0ca0aedb28b
feridalirzazade <alirzazadef@gmail.com>
2025-11-13 21:19:46 +0400

    stringconcat

---
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -0,0 +1,8 @@
+public class PrintStringConcat {
+    public static void main(String[] args) {
+        String text1 = "Holberton ";
+        String text2 = "School";
+        System.out.println("Welcome to " + text1 + text2 + " !");
+
+    }
+}
