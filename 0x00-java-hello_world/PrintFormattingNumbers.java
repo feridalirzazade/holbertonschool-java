4b89bcb9b1458d6d6a2c6e3e1d12de23b63ec8ff
feridalirzazade <alirzazadef@gmail.com>
2025-11-13 21:30:59 +0400

    complitecode

--- 0x00-java-hello_world/PrintFormattingNumbers.java
+++ 0x00-java-hello_world/PrintFormattingNumbers.java
@@ -2,7 +2,8 @@ public class PrintFormattingNumbers {
     public static void main(String[] args) {
         float tax = 0.2456f;
         float value = 7654.321f;
+        char s = '%';
         System.out.printf("Value: $%.2f\n",value);
-        System.out.printf("Tax: %.2f",tax);
+        System.out.printf("Tax: %.2f%c",tax , s);
     }
 }
