38105b72bcdef76b1b1d0acb69438d5784c3602c
feridalirzazade <alirzazadef@gmail.com>
2025-11-13 21:11:02 +0400

    asd

---
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -0,0 +1,6 @@
+public class PrintStringVariable {
+    public static void main(String[] args) {
+        String text = "Holberton School";
+        System.out.println(text);System.out.println(text);System.out.println(text.substring(0, 9));
+    }
+}
