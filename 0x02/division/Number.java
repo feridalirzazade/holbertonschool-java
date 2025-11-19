01e6adfff82ce5effc89a6bd630046108b17b102
feridalirzazade <alirzazadef@gmail.com>
2025-11-19 21:58:10 +0400

    division

---
+++ 0x02/division/Number.java
@@ -0,0 +1,19 @@
+package division;
+
+public class Number {
+
+
+    public static void todivide(int a, int b){
+
+        int result = 0;
+        try{
+
+            result = a/b;
+        } catch (ArithmeticException e) {
+            System.out.println("It is not possible to divide by zero.");
+        }finally {
+
+            System.out.println(a + " / " + b + " = " + result);
+        }
+    }
+}
