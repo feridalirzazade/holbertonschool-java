8163cbe40bdd747d0c46305e7200b981408853f6
feridalirzazade <alirzazadef@gmail.com>
2025-11-21 22:10:26 +0400

    bookstore

--- 0x03/bookstore/Program.java
+++ 0x03/bookstore/Program.java
@@ -1,5 +1,8 @@
+import java.util.Locale;
+
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.GERMANY);
 
         try {
             Book book1 = new Book(
