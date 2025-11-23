953f9f472739c7a98e555d4c199bdb7a0476b7a4
feridalirzazade <alirzazadef@gmail.com>
2025-11-23 22:22:42 +0400

    ch4

--- Classes_Objects/status/CharacterGame.java
+++ Classes_Objects/status/CharacterGame.java
@@ -1,13 +1,19 @@
 public class CharacterGame {
     private int currentHealth;
     private String name;
+    private String status;
 
     public int getCurrentHealth() {
         return currentHealth;
     }
 
+    public String getStatus(){
+        return status;
+    }
     public void setCurrentHealth(int currentHealth) {
         this.currentHealth = currentHealth;
+        if(currentHealth > 0) status = "alive";
+        else status = "dead";
     }
 
     public String getName() {
