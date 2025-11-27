de1e0d79e969bf54808f4ce2bec450bc0370014a
feridalirzazade <alirzazadef@gmail.com>
2025-11-27 21:13:38 +0400

    summary

--- Inheritance_exercises/order_summary/Order.java
+++ Inheritance_exercises/order_summary/Order.java
@@ -10,41 +10,27 @@ public class Order {
     }
 
     public void presentOrderSummary() {
+        System.out.println("------- ORDER SUMMARY ------");
 
-        System.out.println("------- ORDER SUMMARY -------");
-
-        double totalProducts = 0.0;
+        double totalProducts = 0;
 
         for (ItemOrder item : items) {
-
             String type = item.getProduct().getClass().getSimpleName();
             String title = item.getProduct().getTitle();
             double price = item.getProduct().getNetPrice();
-            int quant = item.getQuantity();
-            double total = price * quant;
-
+            int quantity = item.getQuantity();
+            double total = price * quantity;
             totalProducts += total;
 
-            System.out.printf(
-                    Locale.FRANCE,
-                    "Type: %s  Title: %s  Price: %.2f  Quant: %d  Total: %.2f%n",
-                    type, title, price, quant, total
-            );
+            System.out.printf("Type: %s  Title: %s  Price: %.2f  Quant: %d  Total: %.2f\n",
+                    type, title, price, quantity, total);
         }
-
         System.out.println("----------------------------");
-
-        double discountValue = totalProducts * (discountPercentage / 100);
-
-        System.out.printf(Locale.FRANCE, "DISCOUNT: %.2f%n", discountValue);
-        System.out.printf(Locale.FRANCE, "TOTAL PRODUCTS: %.2f%n", totalProducts);
-
+        double discountAmount = totalProducts * discountPercentage / 100.0;
+        System.out.printf("DISCOUNT: %.2f\n", discountAmount);
+        System.out.printf("TOTAL PRODUCTS: %.2f\n", totalProducts);
         System.out.println("----------------------------");
-
-        double finalTotal = totalProducts - discountValue;
-
-        System.out.printf(Locale.FRANCE, "TOTAL ORDER: %.2f%n", finalTotal);
-
+        System.out.printf("TOTAL ORDER: %.2f\n", totalProducts - discountAmount);
         System.out.println("----------------------------");
     }
     public double calculateTotal(){
