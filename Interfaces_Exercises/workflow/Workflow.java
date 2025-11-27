c49a3e90af41646d7afc67badb834be9b75657a6
feridalirzazade <alirzazadef@gmail.com>
2025-11-28 00:44:32 +0400

    workflow

---
+++ Interfaces_Exercises/workflow/Workflow.java
@@ -0,0 +1,11 @@
+import activity.Activity;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Workflow {
+    public List<Activity> activities = new ArrayList<>();
+
+    public void registerActivity(Activity activity){
+        activities.add(activity);
+    }
+}
