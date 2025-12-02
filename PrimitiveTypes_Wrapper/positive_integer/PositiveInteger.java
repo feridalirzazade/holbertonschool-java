c17322ce917b9df0f0589a4e00c4ec99b164d08d
feridalirzazade <alirzazadef@gmail.com>
2025-12-02 04:27:07 +0400

    positive_integer

---
+++ PrimitiveTypes_Wrapper/positive_integer/PositiveInteger.java
@@ -0,0 +1,31 @@
+public class PositiveInteger {
+    int value;
+    PositiveInteger(int value){
+        if(value > 0) this.value = value;
+        else throw new IllegalArgumentException("Value is not a positive integer");
+
+    }
+    PositiveInteger(String value){
+        int value1 = Integer.parseInt(value);
+        if( value1> 0) this.value = value1;
+        else throw new IllegalArgumentException("Value is not a positive integer");
+    }
+    public boolean isPrime() {
+        if (value <= 1) return false;
+
+        for (int i = 2; i < value; i++) {
+            if (value % i == 0) return false;
+        }
+
+        return true;
+    }
+    public int getValue(){
+        return value;
+    }
+    public void setValue(int value) {
+        this.value = value;
+    }
+    public void setValor(int value) {
+        this.value = value;
+    }
+}
